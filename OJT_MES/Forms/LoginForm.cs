b49f611b131cd5be36800408f3892c31e633de33
ykPark <puklove22@naver.com>
2025-04-18 16:28:50 +0900

    Feat: Login Form 일부구현

---
+++ OJT_MES/Forms/LoginForm.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using LotteMES.Bases;
+using LotteMES.FormData;
+using LotteMES.Styles;
+using LotteMES.Helpers;
+
+namespace LotteMES.Forms
+{
+    public partial class LoginForm : MESFormBase
+    {
+
+        public LoginForm()
+        {
+            InitializeComponent();
+
+            // 테두리 제거
+            this.FormBorderStyle = FormBorderStyle.None;
+
+            SetStyles();
+        }
+    }
+}
