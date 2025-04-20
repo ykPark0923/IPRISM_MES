7baf82d544f1b3492f74ff0294f04c85461837c9
ykPark <puklove22@naver.com>
2025-04-21 08:52:53 +0900

    Feat : 기본정보생성 Form 일부 구현

---
+++ OJT_MES/Forms/SettingBasicInfoForm.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace LotteMES.Forms
+{
+    partial class SettingBasicInfoForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Number = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ProcessName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.LineCode = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.LineName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(36, 146);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 20);
+            this.comboBox1.TabIndex = 23;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("굴림", 10F);
+            this.label4.Location = new System.Drawing.Point(57, 115);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(96, 14);
+            this.label4.TabIndex = 19;
+            this.label4.Text = "기본코드 조회";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(552, 96);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(110, 70);
+            this.button3.TabIndex = 27;
+            this.button3.Text = "닫기";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(427, 96);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(120, 70);
+            this.button2.TabIndex = 26;
+            this.button2.Text = "라인정보\r\n다운로드";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(304, 96);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(117, 70);
+            this.button1.TabIndex = 25;
+            this.button1.Text = "제품정보\r\n다운로드";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("굴림", 35F);
+            this.label1.Location = new System.Drawing.Point(12, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(318, 47);
+            this.label1.TabIndex = 24;
+            this.label1.Text = "기본정보 설정";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Number,
+            this.ProcessName,
+            this.LineCode,
+            this.LineName});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 203);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(642, 407);
+            this.dataGridView1.TabIndex = 28;
+            // 
+            // Number
+            // 
+            this.Number.HeaderText = "No";
+            this.Number.Name = "Number";
+            // 
+            // ProcessName
+            // 
+            this.ProcessName.HeaderText = "공정명";
+            this.ProcessName.Name = "ProcessName";
+            // 
+            // LineCode
+            // 
+            this.LineCode.HeaderText = "라인코드";
+            this.LineCode.Name = "LineCode";
+            // 
+            // LineName
+            // 
+            this.LineName.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.LineName.HeaderText = "라인명";
+            this.LineName.Name = "LineName";
+            // 
+            // CreateBasicInfoForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(675, 622);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button1);
+            this.Name = "CreateBasicInfoForm";
+            this.Text = "CreateBasicInfoForm";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Number;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ProcessName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn LineCode;
+        private System.Windows.Forms.DataGridViewTextBoxColumn LineName;
+    }
+}
